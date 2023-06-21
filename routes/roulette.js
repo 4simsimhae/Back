ee48445b226422f7c57210f7ed3d521dfcfcb1d8
Bbabam <sv741147vs@gmail.com>
2023-06-21 23:53:23 +0900

    보내는 형식 변경 /룰렛

--- routes/roulette.js
+++ routes/roulette.js
@@ -7,9 +7,17 @@ const router = express.Router();
 require('dotenv').config();
 const { Configuration, OpenAIApi } = require('openai');
 
+// 응답 객체
+class ApiResponse {
+    constructor(code, message = '', data = {}) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+}
+
 //chatGPT 오픈 API 호출
 async function callChatGPT(prompt) {
-
     const configuration = new Configuration({
         apiKey: process.env.OPENAI_API_KEY,
     });
@@ -17,7 +25,7 @@ async function callChatGPT(prompt) {
     try {
         const openai = new OpenAIApi(configuration);
         const response = await openai.createChatCompletion({
-            model: "gpt-3.5-turbo",
+            model: 'gpt-3.5-turbo',
             messages: prompt,
             temperature: 0.8,
         });
@@ -33,28 +41,32 @@ async function callChatGPT(prompt) {
     }
 }
 
-
 //chatGPT로 질문 8개 받아오기
 router.post('/chatgpt', async (req, res) => {
-    try{
+    try {
         //여기 아래에 있는 문장을 적절하게 수정하여
         //우리가 원하는 형식의 질문을 받아야합니다.
-        const {ask} = {
-            "ask" : '연애 카테고리에 대한 황당하고 엽기스러운 토론 주제 8가지를 json 형식으로 새로 추천해줘'
-        }
+        const { ask } = {
+            ask: '연애 카테고리에 대한 황당하고 엽기스러운 토론 주제 8가지를 새로 추천해줘',
+        };
         const reply = await callChatGPT([{ role: 'user', content: ask }]);
 
-        if (reply) { //응답 들어있는지 확인
-            res.json({reply});
+        if (reply) {
+            //응답 들어있는지 확인
+            res.json({ reply });
             //질문 몇개 DB에 저장하기 코드 추가예정
         } else {
             //DB에 저장되어있는 파일 불러오기 코드 추가예정
-            res.json({reply});
+            res.json({ reply });
         }
-
-    }catch (error) {
-        console.log(error)
-        return res.status(500).json('예상하지 못한 서버 문제가 발생했습니다.');
+        const response = new ApiResponse(200, '', { reply: reply });
+        return res.status(200).json(response);
+    } catch (error) {
+        const response = new ApiResponse(
+            500,
+            '예상하지 못한 서버 문제가 발생했습니다.'
+        );
+        return res.status(500).json(response);
     }
 });
 
