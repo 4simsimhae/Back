104a2f1c4c045ea1734962e8cd83b997e4f06255
Bbabam <sv741147vs@gmail.com>
2023-06-23 23:45:22 +0900

    카테고리 목록

--- routes/room.js
+++ routes/room.js
@@ -1,38 +1,113 @@
 const express = require('express');
 const router = express.Router();
-//const { User, Kategorie, UseInfo, Room, subject, chat } = require('../models');
+const { User, Kategorie, UseInfo, Room, subject, chat } = require('../models');
 //const checkLogin = require('../middlewares/checkLogin.js'); //유저아이디받기
 
+// 응답 객체
+class ApiResponse {
+    constructor(code, message = '', data = {}) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+}
 
+//카테고리 목록
+router.get('/kategorie', async (req, res) => {
+    try{
+        const kategorielist = await Kategorie.findAll({
+            attributes: ['KategorieId', 'KategorieName'],
+            //order: [],
+        });
+        
+        const response = new ApiResponse(200, '', kategorielist);
+        return res.status(200).json(response);
+
+    } catch (error) {
+        const response = new ApiResponse(
+            500,
+            '예상하지 못한 서버 문제가 발생했습니다.'
+        );
+        return res.status(500).json(response);
+    }
+});
 
 //게임 방 리스트
 router.get('/roomlist/:kategorieId', async (req, res) => {
-    res.status(200).send('게임 방 리스트 확인하기 api');
+    try{
+
+    } catch (error) {
+        const response = new ApiResponse(
+            500,
+            '예상하지 못한 서버 문제가 발생했습니다.'
+        );
+        return res.status(500).json(response);
+    }
 });
 
 //게임 방 상세정보
 router.get('/roomlist/:roomId', async (req, res) => {
-    res.status(200).send('게임 방 상세정보 확인하기 api');
+    try{
+
+    } catch (error) {
+        const response = new ApiResponse(
+            500,
+            '예상하지 못한 서버 문제가 발생했습니다.'
+        );
+        return res.status(500).json(response);
+    }
 });
 
 //게임 방 만들기
 router.post('/roomlist/:kategorieId', async (req, res) => {
-    res.status(200).send('게임 방 만들기 api');
+    try{
+
+    } catch (error) {
+        const response = new ApiResponse(
+            500,
+            '예상하지 못한 서버 문제가 발생했습니다.'
+        );
+        return res.status(500).json(response);
+    }
 });
 
 //배심원으로 참여하기
 router.put('/jury/:roomId', async (req, res) => {
-    res.status(200).send('배심원으로 참여하기 api');
+    try{
+
+    } catch (error) {
+        const response = new ApiResponse(
+            500,
+            '예상하지 못한 서버 문제가 발생했습니다.'
+        );
+        return res.status(500).json(response);
+    }
 });
 
 //토론자로 참여하기
 router.put('/discussant/:roomId', async (req, res) => {
-    res.status(200).send('토론자로 참여하기 api');
+    try{
+
+    } catch (error) {
+        const response = new ApiResponse(
+            500,
+            '예상하지 못한 서버 문제가 발생했습니다.'
+        );
+        return res.status(500).json(response);
+    }
 });
 
 //방 삭제하기
 router.delete('/roomlist/:roomId', async (req, res) => {
-    res.status(200).send('방 삭제하기 api');
+    try{
+
+    } catch (error) {
+        const response = new ApiResponse(
+            500,
+            '예상하지 못한 서버 문제가 발생했습니다.'
+        );
+        return res.status(500).json(response);
+    }
 });
 
 
