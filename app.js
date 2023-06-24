584833918e16c1567e4ba0d249d2e92d4e07590c
Bbabam <sv741147vs@gmail.com>
2023-06-24 22:03:29 +0900

    app.js수정

--- app.js
+++ app.js
@@ -45,12 +45,12 @@ app.use(
         secret: process.env.SESSION_SECRET,
         resave: false,
         saveUninitialized: false,
-        cookie: {
-            domain: 'http://localhost:3000',
-            path: '/', // /로 설정하면 모든 페이지에서 쿠키를 사용할 수 있습니다.
-            secure: false, // https가 아닌 환경에서도 사용할 수 있습니다.
-            httpOnly: false, // 자바스크립트에서 쿠키를 확인할 수 있습니다.
-        },
+        // cookie: {
+        //     domain: 'http://localhost:3000',
+        //     path: '/', // /로 설정하면 모든 페이지에서 쿠키를 사용할 수 있습니다.
+        //     secure: false, // https가 아닌 환경에서도 사용할 수 있습니다.
+        //     httpOnly: false, // 자바스크립트에서 쿠키를 확인할 수 있습니다.
+        // },
     })
 );
 
