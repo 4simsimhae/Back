c2a5bd5bd6ff4db911c4fa3314d835b7c6d72462
Bbabam <sv741147vs@gmail.com>
2023-06-24 21:26:43 +0900

    domaingit add .!

--- routes/auth.js
+++ routes/auth.js
@@ -30,11 +30,11 @@ router.get(
             res.locals.token = token;
             res.cookie('Authorization', `Bearer ${token}`),
             {
-                secure: true,
-                maxAge: 3600000,
-                httpOnly: false,
-                sameSite: 'none',
-                domain: 'simsimhae.store',
+                domain: "front-black-delta.vercel.app",
+                secure: false,
+                sameSite: "strict",
+                httpOnly: true,
+                maxAge: 1000 * 60 * 60 * 24, //1 day
             };
 
             // res.redirect(
