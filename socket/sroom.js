ee26b155d66e8996999c2c78ac320a37185468c8
Bbabam <sv741147vs@gmail.com>
2023-06-25 19:18:39 +0900

    배포테스트

--- socket/sroom.js
+++ socket/sroom.js
@@ -3,30 +3,53 @@ const axios = require('axios');
 
 module.exports = (io) => {
     io.on('connection', (socket) => {
+        socket.onAny((event) => {
+            console.log(`Socket Event: ${event}`);
+        });
         // 토론자로 참여하기
-        socket.on('joinDebate', async (userId) => {
+        socket.on('joinDebate', async (userId, roomId, done) => {
             try {
                 // userId 조회
                 const user = await UserInfo.findOne({
                     where: { userId },
                 });
+                console.log('1 userId = ', userId);
 
                 if (!user) {
                     socket.emit('error', '유저를 찾을 수 없습니다.');
                     return;
                 }
 
+                // roomId로 선택한방 조회
+                const room = await Room.findOne({
+                    where: { roomId },
+                });
+                console.log('2 roomId =', room.roomId);
+
+                if (!room) {
+                    socket.emit('error', '입장할 수 있는 방이 없습니다.');
+                    return;
+                }
+
+                // room에 입장
+                socket.join(room.roomId);
+                socket.roomId = room.roomId;
+                console.log('3 roomId =', room.roomId);
+
                 // debater 수정 및 DB에 저장
                 user.debater = 1;
                 await user.save();
 
-                // 클라이언트에게 정보 전달
-                socket.emit('debateJoined', {
-                    userId,
-                    roomId: socket.roomId,
-                    debater: user.debater,
-                    nickname: user.nickname,
-                });
+                // 랜덤 닉네임 가져오기
+                const nickname = await getRandomNickname();
+
+                // 닉네임 수정 및 DB에 저장
+                user.nickname = nickname;
+                await user.save();
+
+                done();
+
+                socket.emit('debateJoined', { userId, nickname });
             } catch (error) {
                 console.error('토론 참여 처리 실패:', error);
                 socket.emit('error', '토론 참여 처리에 실패했습니다.');
@@ -34,17 +57,38 @@ module.exports = (io) => {
         });
 
         // 배심원으로 참가하기
-        socket.on('joinJuror', async (userId,done) => {
+        socket.on('joinJuror', async (userId, roomId, done) => {
             try {
                 // userId 조회
                 const user = await UserInfo.findOne({
                     where: { userId },
                 });
+                console.log('1 userId = ', userId);
 
                 if (!user) {
                     socket.emit('error', '유저를 찾을 수 없습니다.');
                     return;
                 }
+
+                // roomId로 선택한방 조회
+                const room = await Room.findOne({
+                    where: { roomId },
+                });
+                console.log('2 roomId =', room.roomId);
+
+                if (!room) {
+                    socket.emit('error', '입장할 수 있는 방이 없습니다.');
+                    return;
+                }
+
+                // room에 입장
+                socket.join(room.roomId);
+                socket.roomId = room.roomId;
+                console.log('3 roomId =', room.roomId);
+
+                // debater 수정 및 DB에 저장
+                user.debater = 0;
+                await user.save();
 
                 // 랜덤 닉네임 가져오기
                 const nickname = await getRandomNickname();
