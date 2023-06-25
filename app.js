47daa0dcb0edb988641232adf33c8faa57990e13
Bbabam <sv741147vs@gmail.com>
2023-06-25 17:25:46 +0900

    testtest

--- app.js
+++ app.js
@@ -1,55 +1,42 @@
-require('dotenv').config(); //환경변수를 .env로 관리
+require('dotenv').config();
 const express = require('express');
 const kakao = require('./passport/KakaoStrategy');
 const cookieParser = require('cookie-parser');
 const app = express();
 const passport = require('passport');
-const http = require("http");
+const http = require('http');
 const server = http.createServer(app);
 
-const socketHandlers = require("./socket");
-
-
-socketHandlers(io);
-
 //swagger
-const swaggerUi = require('swagger-ui-express'); // swagger
+const swaggerUi = require('swagger-ui-express');
 const swaggerDocs = require('./swagger.js');
-/**
- *  @swagger
- *  tag :
- *      name:
- *      description :
- */
 
 const indexRouter = require('./routes/index.js');
 const session = require('express-session');
-const authRouter = require('./routes/auth.js'); //URL 때문에 index에 안합치고 따로 빼서 구현 했어요!!
+const authRouter = require('./routes/auth.js');
 
 app.use(express.json());
-app.use('/docs-api', swaggerUi.serve, swaggerUi.setup(swaggerDocs)); //스웨거 확인 api
+app.use('/docs-api', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-
-
-const io = require("socket.io")(server)
-// {
-//     cors: {
-//     origin: ['https://simsimhae.store', 'http://localhost:3000'],
-//     credentials: true,
-// },
-// })
 
 //CORS 설정
 const cors = require('cors');
 app.use(
     cors({
-        origin: ['https://simsimhae.store', 'http://localhost:3000', 'https://front-black-delta.vercel.app/'],
-
+        origin: ['https://simsimhae.store', 'http://localhost:3000'],
         credentials: true,
     })
 );
+
+const io = require('socket.io')(server, {
+    cors: {
+        origin: ['https://simsimhae.store', 'http://localhost:3000'],
+        credentials: true,
+    },
+});
+
+const socketHandlers = require('./socket');
 
 app.use(
     session({
@@ -57,10 +44,10 @@ app.use(
         resave: false,
         saveUninitialized: false,
         cookie: {
-            domain: 'http://localhost:3000',//'https://front-black-delta.vercel.app/',
-            path: '/', // /로 설정하면 모든 페이지에서 쿠키를 사용할 수 있습니다.
-            secure: false, // https가 아닌 환경에서도 사용할 수 있습니다.
-            httpOnly: false, // 자바스크립트에서 쿠키를 확인할 수 있습니다.
+            domain: 'http://localhost:3000',
+            path: '/',
+            secure: false,
+            httpOnly: false,
         },
     })
 );
@@ -74,15 +61,12 @@ passport.serializeUser((token, done) => {
 
 passport.deserializeUser((token, done) => {
     try {
-        // 토큰을 이용하여 사용자를 인증 또는 사용자 정보를 가져오는 로직 구현
-        // 예시: 토큰에서 userId를 추출하여 사용자 정보를 가져옴
-        console.log('app.js에 담겨 decoded된 토큰 = ', token);
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         const userId = decoded.userId;
 
         Users.findByPk(userId)
             .then((user) => {
-                done(null, user); // 사용자 객체를 세션에서 가져옴
+                done(null, user);
             })
             .catch((err) => {
                 done(err);
@@ -96,9 +80,7 @@ passport.deserializeUser((token, done) => {
     }
 });
 
-kakao(); // kakaoStrategy.js의 module.exports를 실행합니다.
-
-
+kakao();
 
 app.use('/', authRouter);
 app.use('/api', [indexRouter]);
@@ -107,6 +89,8 @@ app.get('/', (req, res) => {
     res.status(200).send('simsimhae API / Use "/docs-api" Page');
 });
 
+socketHandlers(io);
+
 server.listen(3000, () => {
     console.log('3000 포트로 서버 연결');
-}); //
+});
