6e8556a652d5ba0702fc631ce03789a067bdbe67
Bbabam <sv741147vs@gmail.com>
2023-07-01 15:39:30 +0900

    node-cron

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express();
 const passport = require('passport');
 const http = require('http');
 const server = http.createServer(app); //
+var cron = require('node-cron');
+const { Kategorie, Subject } = require('./models');
 
 //swagger
 const swaggerUi = require('swagger-ui-express');
@@ -102,4 +104,50 @@ socketHandlers(io);
 
 server.listen(3000, () => {
     console.log('3000 포트로 서버 연결');
+});
+
+
+//매일 자정에 chatGPT를 이용하여 새로운 주제 받기
+// 요일 이름과 id값을 저장한 배열
+const days = [
+    { id: 1, name: "Mon", toMinus: 7 },
+    { id: 2, name: "Tue", toMinus: 1 },
+    { id: 3, name: "Wed", toMinus: 2 },
+    { id: 4, name: "Thu", toMinus: 3 },
+    { id: 5, name: "Fri", toMinus: 4 },
+    { id: 6, name: "Sat", toMinus: 5 },
+    { id: 7, name: "Sun", toMinus: 6 },
+];
+// second minute hour day-of-month month day-of-week
+days.forEach((day) => {
+    cron.schedule(`0 0 0 * * ${day.name}`, async () => {
+        try {
+            const kategorieId  = day.id;
+            const { kategorieName } = await Kategorie.findOne({
+                attributes: ['kategorieName'],
+                where: { kategorieId },
+        });
+
+        //GPT에 질문하기
+        const [ kategorieName1, kategorieName2] = kategorieName.split(' ');
+        const { ask } = {
+            ask: `${kategorieName1} 혹은 ${kategorieName2} 카테고리에 대한 황당하고 엽기스러운 VS 형식의 토론 주제 100가지를 숫자 없이 큰따옴표 안에 주제만 적어서 배열 형식으로 새로 나열해줘.`,
+        };
+
+        const reply = await callChatGPT([{ role: 'user', content: ask }]);
+        console.log(reply.content);
+
+        const subjectList = reply.content //+ subject.subjectList;
+        const roomlist = await Subject.update(
+            {
+                subjectList,
+            },
+            {
+                where: { kategorieId },
+            }
+        );
+        } catch (err) {
+            console.error(err);
+        }
+    });
 });
--- routes/roulette.js
+++ routes/roulette.js
@@ -40,65 +40,16 @@ async function callChatGPT(prompt) {
     }
 }
 
-// //chatGPT에 질문하기 (기본 테스트창)//
-// router.get('/chatgpt', async (req, res) => {
-//     try {
-//         const { ask } = {
-//             ask: `여기에 테스트용으로 하고싶은 말을 적으면 됩니다`,
-//         };
-//         const reply = await callChatGPT([{ role: 'user', content: ask }]);
-
-//         if (reply && reply.content) {
-            //응답 들어있는지 확인
-        //     const content = JSON.parse(reply.content);
-        //     const objectReply = Object.values(content);
-        //     res.json({ role: 'user', reply: objectReply });
-        //     //질문 몇개 DB에 저장하기 코드 추가예정
-        // } else {
-        //     //DB에 저장되어있는 파일 불러오기 코드 추가예정
-        //     res.json(reply);
-        // }
-//         const response = new ApiResponse(200, '', { reply: reply });
-//         return res.status(200).json(response);
-//     } catch (error) {
-//         const response = new ApiResponse(
-//             500,
-//             '예상하지 못한 서버 문제가 발생했습니다.'
-//         );
-//         return res.status(500).json(response);
-//     }
-// });
-
-//카테고리 별 주제 8개 받기
-router.post('/chatgpt/:kategorieId', async (req, res) => {
+//chatGPT에 질문하기 (기본 테스트창)//
+router.get('/chatgpt', async (req, res) => {
     try {
-        const { kategorieId } = req.params;
-        const { kategorieName } = await Kategorie.findOne({
-            attributes: ['kategorieName'],
-            where: { kategorieId },
-        });
-
-        //GPT에 질문하기
-        const [ kategorieName1, kategorieName2] = kategorieName.split(' ');
         const { ask } = {
-            ask: `${kategorieName1} 혹은 ${kategorieName2} 카테고리에 대한 황당하고 엽기스러운 VS 형식의 토론 주제 100가지를 숫자 없이 큰따옴표 안에 주제만 적어서 배열 형식으로 새로 나열해줘.`,
+            ask: `여기에 테스트용으로 하고싶은 말을 적으면 됩니다`,
         };
-
         const reply = await callChatGPT([{ role: 'user', content: ask }]);
-        console.log(reply.content);
 
-        const subjectList = reply.content //+ subject.subjectList;
-        const roomlist = await Subject.update(
-            {
-                subjectList,
-            },
-            {
-                where: { kategorieId },
-            }
-        );
-        res.json({ roomlist });
-        //질문 몇개 DB에 저장하기 코드 추가예정
-        
+        const response = new ApiResponse(200, '', { reply: reply });
+        return res.status(200).json(response);
     } catch (error) {
         const response = new ApiResponse(
             500,
@@ -107,6 +58,45 @@ router.post('/chatgpt/:kategorieId', async (req, res) => {
         return res.status(500).json(response);
     }
 });
+
+// //카테고리 별 주제 8개 받기
+// router.post('/chatgpt/:kategorieId', async (req, res) => {
+//     try {
+//         const { kategorieId } = req.params;
+//         const { kategorieName } = await Kategorie.findOne({
+//             attributes: ['kategorieName'],
+//             where: { kategorieId },
+//         });
+
+//         //GPT에 질문하기
+//         const [ kategorieName1, kategorieName2] = kategorieName.split(' ');
+//         const { ask } = {
+//             ask: `${kategorieName1} 혹은 ${kategorieName2} 카테고리에 대한 황당하고 엽기스러운 VS 형식의 토론 주제 100가지를 숫자 없이 큰따옴표 안에 주제만 적어서 배열 형식으로 새로 나열해줘.`,
+//         };
+
+//         const reply = await callChatGPT([{ role: 'user', content: ask }]);
+//         console.log(reply.content);
+
+//         const subjectList = reply.content //+ subject.subjectList;
+//         const roomlist = await Subject.update(
+//             {
+//                 subjectList,
+//             },
+//             {
+//                 where: { kategorieId },
+//             }
+//         );
+//         res.json({ roomlist });
+//         //질문 몇개 DB에 저장하기 코드 추가예정
+        
+//     } catch (error) {
+//         const response = new ApiResponse(
+//             500,
+//             '예상하지 못한 서버 문제가 발생했습니다.'
+//         );
+//         return res.status(500).json(response);
+//     }
+// });
 
 //선택된 주제 받기//
 router.put('/chatgpt/:roomId', async (req, res) => {
