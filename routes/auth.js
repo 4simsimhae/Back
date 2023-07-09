09b7d7001a67d5da26e42a351cd778d7dcba5c28
Bbabam <sv741147vs@gmail.com>
2023-07-09 23:37:31 +0900

    배포용으로 수정

--- routes/auth.js
+++ routes/auth.js
@@ -30,10 +30,10 @@ router.get(
             res.locals.token = token;
             console.log('이건 쿼리에 담긴 토큰 = ', token);
 
-            // res.redirect(
-            //     `https://front-black-delta.vercel.app/auth/kakao/callback/${query}`
-            // );
-            res.redirect(`http://localhost:3000/auth/kakao/callback/${query}`);
+            res.redirect(
+                `https://front-black-delta.vercel.app/auth/kakao/callback/${query}`
+            );
+            // res.redirect(`http://localhost:3000/auth/kakao/callback/${query}`);
         } catch (error) {
             const response = new ApiResponse(
                 500,
