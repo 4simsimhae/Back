a4255f36ee861a3ec4f832476cd71c3f439182aa
Bbabam <sv741147vs@gmail.com>
2023-07-14 21:45:24 +0900

    test

--- mediasoup/mediasoup.js
+++ mediasoup/mediasoup.js
@@ -239,7 +239,7 @@ const createWebRtcTransport = async (callback) => {
             listenIps: [
             {
                 ip: '0.0.0.0', //'172.25.144.1', // replace with relevant IP address
-                announcedIp: '3.39.254.76', //'172.31.0.0/16', //'127.0.0.1', //'3.39.254.76',
+                announcedIp: 'simsimhae.store', //'172.31.0.0/16', //'127.0.0.1', //'3.39.254.76',
             }
             ],
             enableUdp: true,
