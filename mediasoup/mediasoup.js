ee69f7c9f05c29392f054fc54d70408551cbe2d8
Bbabam <sv741147vs@gmail.com>
2023-07-18 02:11:41 +0900

    docker 기본 port

--- mediasoup/mediasoup.js
+++ mediasoup/mediasoup.js
@@ -414,7 +414,7 @@ connections.on('connection', async socket => {
         if (process.platform === "linux") {
             listenip = "0.0.0.0";
             announceip = "127.17.0.1"; 
-            //"3.39.21.142" //인스턴스 퍼블릭 "3.39.254.76" //인스턴스 프라이빗 "172.31.12.132" //VPC IPv4 CIDR "172.31.0.0/16"
+            //"3.39.21.142" //인스턴스 퍼블릭 "3.39.254.76" //인스턴스 프라이빗 "172.31.12.132" //VPC IPv4 CIDR "172.31.0.0/16" //Docker 기본 port "127.17.0.1"
         } else {
             listenip = "192.168.0.16";
             //announceip = "192.168.0.16";
